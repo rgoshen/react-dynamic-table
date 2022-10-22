9a2016acd8c6784e387f36c7c024f3d83356a352
rgoshen <rick.goshen@gmail.com>
2022-10-22 15:23:55 -0700

    add single input to basic form

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,13 @@ module.exports = {
     commonjs: true,
     es2021: true,
     es6: true,
+    node: true,
   },
   extends: ["eslint:recommended", "plugin:react/recommended"],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
+    sourceType: "module",
   },
   plugins: ["react"],
   rules: {
